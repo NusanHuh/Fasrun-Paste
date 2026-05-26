20bb1020d2dcb5d1efe5088dda9a20e23bfaeff4
NusanHuh <adityaparama119@gmail.com>
2026-05-27 03:53:52 +0700

    Komit pertamaku

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.ico';
+	
+	import "@fontsource/mona-sans/400.css";
+	import "@fontsource/mona-sans/500.css";
+	import "@fontsource/mona-sans/600.css";
+	import "@fontsource/mona-sans/700.css";
+	import "@fontsource/mona-sans/900.css";
+
+	import "@fontsource/fira-code/300.css";
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+{@render children()}
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Halo ini login page</h1>
