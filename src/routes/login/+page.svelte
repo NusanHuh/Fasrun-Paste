972d6dbeb338d4b4a74266ee5acaf115d9e94f37
NusanHuh <adityaparama119@gmail.com>
2026-05-28 20:12:50 +0700

    update login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,147 @@
-<h1>Halo ini login page</h1>
+<main class="w-full h-screen overflow-hidden grid grid-cols-2 max-sm:grid-cols-1 max-sm:overflow-auto max-sm:h-fit">
+    <div class="w-full h-full bg-[#000000] flex items-start justify-center px-17.5 py-17.5 relative max-sm:px-7.5 max-sm:pb-50 max-sm:pt-7.5">
+        <div class="w-full max-w-100 h-fit flex flex-col items-start justify-start">
+            <h1 class="w-fit text-left text-[#ffffff] text-[30px] font-semibold">Buat akun anda gratis</h1>
+            <p class="text-[#ffffff] text-[13px] mt-5">Gunakan Fasrun Paste sesuka anda, dengan mendaftar anda baru saja memulai penggunaan.</p>
+
+            <p class="w-fit text-[#ffffff] text-[14px] mt-5 font-semibold flex items-center justify-start gap-1.25 hover:border-b-2 border-br-[#ffffff] cursor-pointer z-100">Sudah mempunyai akun?<button class="cursor-pointer">Masuk</button><svg class="w-4.25 fill-[#ffffff] cursor-pointer" id="angle-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><polygon points="16 11 16 13 15 13 15 14 14 14 14 15 13 15 13 16 12 16 12 17 11 17 11 18 10 18 10 19 9 19 9 20 8 20 8 19 7 19 7 18 8 18 8 17 9 17 9 16 10 16 10 15 11 15 11 14 12 14 12 13 13 13 13 11 12 11 12 10 11 10 11 9 10 9 10 8 9 8 9 7 8 7 8 6 7 6 7 5 8 5 8 4 9 4 9 5 10 5 10 6 11 6 11 7 12 7 12 8 13 8 13 9 14 9 14 10 15 10 15 11 16 11"/></svg></p>
+        </div>
+
+        <svg class="absolute -bottom-17.5 zoom-300 max-sm:zoom-200 z-90" xmlns="http://www.w3.org/2000/svg" width="200" height="200">
+            <g style="order: -1;">
+                <polygon
+                    transform="rotate(45 100 100)"
+                    stroke-width="2"
+                    stroke=""
+                    fill="#414750"
+                    points="70,70 150,50 130,130 50,150"
+                ></polygon>
+                <polygon
+                    stroke-width="2"
+                    stroke=""
+                    fill="url(#gradiente)"
+                    points="100,70 150,100 100,130 50,100"
+                ></polygon>
+                <defs>
+                    <linearGradient y2="100%" x2="10%" y1="0%" x1="0%" id="gradiente">
+                        <stop style="stop-color: #1f883d;stop-opacity:1" offset="20%"></stop>
+                        <stop style="stop-color:#414750;stop-opacity:1" offset="60%"></stop>
+                    </linearGradient>
+                </defs>
+                <polygon
+                    transform="translate(20, 31)"
+                    stroke-width="2"
+                    stroke=""
+                    fill="#1a7233"
+                    points="80,50 80,75 80,99 40,75"
+                ></polygon>
+                <polygon
+                    transform="translate(20, 31)"
+                    stroke-width="2"
+                    stroke=""
+                    fill="url(#gradiente2)"
+                    points="40,-40 80,-40 80,99 40,75"
+                ></polygon>
+                <defs>
+                    <linearGradient y2="100%" x2="0%" y1="-17%" x1="10%" id="gradiente2">
+                        <stop style="stop-color: #1f883d00;stop-opacity:1" offset="20%"></stop>
+                        <stop
+                            style="stop-color:#1f883d54;stop-opacity:1"
+                            offset="100%"
+                            id="animatedStop"
+                        ></stop>
+                    </linearGradient>
+                </defs>
+                <polygon
+                    transform="rotate(180 100 100) translate(20, 20)"
+                    stroke-width="2"
+                    stroke=""
+                    fill="#1f883d"
+                    points="80,50 80,75 80,99 40,75"
+                ></polygon>
+                <polygon
+                    transform="rotate(0 100 100) translate(60, 20)"
+                    stroke-width="2"
+                    stroke=""
+                    fill="url(#gradiente3)"
+                    points="40,-40 80,-40 80,85 40,110.2"
+                ></polygon>
+                <defs>
+                    <linearGradient y2="100%" x2="10%" y1="0%" x1="0%" id="gradiente3">
+                        <stop style="stop-color: #1f883d00;stop-opacity:1" offset="20%"></stop>
+                        <stop
+                            style="stop-color:#1f883d54;stop-opacity:1"
+                            offset="100%"
+                            id="animatedStop"
+                        ></stop>
+                    </linearGradient>
+                </defs>
+                <polygon
+                    transform="rotate(45 100 100) translate(80, 95)"
+                    stroke-width="2"
+                    stroke=""
+                    fill="#ffffff"
+                    points="5,0 5,5 0,5 0,0"
+                    id="particles"
+                ></polygon>
+                <polygon
+                    transform="rotate(45 100 100) translate(80, 55)"
+                    stroke-width="2"
+                    stroke=""
+                    fill="#1f883d"
+                    points="6,0 6,6 0,6 0,0"
+                    id="particles"
+                ></polygon>
+                <polygon
+                    transform="rotate(45 100 100) translate(70, 80)"
+                    stroke-width="2"
+                    stroke=""
+                    fill="#fff"
+                    points="2,0 2,2 0,2 0,0"
+                    id="particles"
+                ></polygon>
+                <polygon
+                    stroke-width="2"
+                    stroke=""
+                    fill="#292d34"
+                    points="29.5,99.8 100,142 100,172 29.5,130"
+                ></polygon>
+                <polygon
+                    transform="translate(50, 92)"
+                    stroke-width="2"
+                    stroke=""
+                    fill="#1f2127"
+                    points="50,50 120.5,8 120.5,35 50,80"
+                ></polygon>
+            </g>
+        </svg>
+    </div>
+
+    <div class="scrolledit w-full h-full bg-[#ffffff] flex flex-col px-25 py-17.5 overflow-auto relative z-100 max-sm:overflow-hidden max-sm:px-7.5 max-sm:py-7.5">
+        <h1 class="w-fit text-left text-[#1f2328] text-[20px] font-semibold">Daftar untuk Fasrun Paste</h1>
+
+        <button class="w-full py-2.5 px-2.5 bg-[#f6f8fa] flex items-center justify-center font-semibold gap-1.75 text-[13.5px] text-[#1f2328] mt-7.5 cursor-pointer rounded-[5px] border-[1.5px] border-[#d5dce3]"><svg class="w-4" id="google" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><polygon points="23 10 23 15 22 15 22 17 21 17 21 19 20 19 20 20 19 20 19 21 17 21 17 22 15 22 15 23 9 23 9 22 7 22 7 21 5 21 5 20 4 20 4 19 3 19 3 17 2 17 2 15 1 15 1 9 2 9 2 7 3 7 3 5 4 5 4 4 5 4 5 3 7 3 7 2 9 2 9 1 15 1 15 2 17 2 17 3 19 3 19 5 18 5 18 6 17 6 17 7 15 7 15 6 9 6 9 7 7 7 7 9 6 9 6 15 7 15 7 17 9 17 9 18 15 18 15 17 17 17 17 15 18 15 18 14 12 14 12 10 23 10"/></svg>Lanjutkan dengan Google</button>
+        
+        <div class="flex items-center justify-center mt-5 mb-5 gap-2.5">
+            <hr class="w-full h-px border-[#d5dce3]">
+            <p class="text-[13px] text-[#1f2328] font-medium">Atau</p>
+            <hr class="w-full h-px border-[#d5dce3]">
+        </div>
+
+        <label for="email" class="leading-3 text-[15px] text-[#1f2328] font-semibold">Email</label>
+        <input class="mt-2.5 rounded-[5px] border-[1.5px] border-[#d5dce3] text-[14px] text-[#1f2328] bg-[#ffffff]" type="email" placeholder="hutao@fasrun.my.id">
+
+        <label for="email" class="leading-3 text-[15px] text-[#1f2328] font-semibold mt-5">Username</label>
+        <input class="mt-2.5 rounded-[5px] border-[1.5px] border-[#d5dce3] text-[14px] text-[#1f2328] bg-[#ffffff]" type="text" placeholder="hutao imutz">
+        <p class="text-[12px] text-[#1f2328] mt-1.75">Harap gunakan nama yang baik dan benar, nama pengguna tidak mengandung unsur sara atau kekerasan lainnya.</p>
+
+        <label for="email" class="leading-3 text-[15px] text-[#1f2328] font-semibold mt-5">Password</label>
+        <input class="mt-2.5 rounded-[5px] border-[1.5px] border-[#d5dce3] text-[14px] text-[#1f2328] bg-[#ffffff]" type="password" placeholder="Password">
+        <p class="text-[12px] text-[#1f2328] mt-1.75">Gunakan password yang kuat dengan minimal 6 karakter apapun untuk membuat password.</p>
+
+        <label for="email" class="leading-3 text-[15px] text-[#1f2328] font-semibold mt-5">Konfirmasi password</label>
+        <input class="mt-2.5 rounded-[5px] border-[1.5px] border-[#d5dce3] text-[14px] text-[#1f2328] bg-[#ffffff]" type="password" placeholder="Konfirmasi">
+
+        <button class="w-full py-2.5 text-center bg-[#1f883d] text-[#ffffff] font-semibold rounded-[5px] mt-7 cursor-pointer">Mendaftar</button>
+    </div>
+</main>
